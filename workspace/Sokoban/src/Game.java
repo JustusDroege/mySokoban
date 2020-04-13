1d5c362384414841a1bb35c7f9ede59c437958c3
justus <justus.droegemueller@tu-braunschweig.de>
2020-04-13 14:23:55 +0200

    game saves string arr of map

--- workspace/Sokoban/src/Game.java
+++ workspace/Sokoban/src/Game.java
@@ -2,16 +2,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.*;
 
 public class Game {
 	public static void run(String field) throws IOException{
+		
 		File folder = new File("level/");
 		File file = new File(folder, field);
-	    FileReader fr = new FileReader(file.getPath());   //Creation of File Reader object
-	    BufferedReader br = new BufferedReader(fr);  //Creation of BufferedReader object
+	    FileReader fr = new FileReader(file.getPath()); 
+	    BufferedReader br = new BufferedReader(fr);  
+	    
 	    int lines = 0;
 	    String l;
+	    int ch;
 	    int max_line_length = 0;
 	    while((l = br.readLine()) != null){
 	    	if(l.length() > max_line_length){
@@ -19,15 +22,28 @@ public class Game {
 	    	}
 	    	lines++;
 	    }
-		char[][] map = new char[lines][max_line_length];
-		for(int i = 0; i < lines; i++){
-			for(int j = 0; j < max_line_length; j++){
-				map[i][j] = '#';
-			}
-		}
-	    System.out.println(Arrays.toString(map[0]));
-	    System.out.println(max_line_length);
-	    br.close();
+	    
+	    char[][] map = new char[lines][max_line_length];
+
+	    FileReader reader = new FileReader(file.getPath());
+	    BufferedReader f = new BufferedReader(reader);
+	    String d;
+	    int k = 0;
+	    while((d = f.readLine()) != null){
+	    	
+	    	for(int i = 0; i< d.length(); i++){
+	    		//System.out.println(d.charAt(i));
+	    		map[k][i] = d.charAt(i);
+	    		//add game logic here
+	    	}
+	    	k++;
+	    }
+	    /*
+	    for(int i = 0; i< lines; i++){
+	    	for(int j = 0; j< max_line_length; j++){
+	    		System.out.println(map[i][j]);
+	    	}
+	    }*/
 	}
 	
 	public static void main(String[] args) throws IOException{
