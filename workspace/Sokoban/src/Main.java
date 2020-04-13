cbdab4dcf6a827fd5bfe689013568c3336d97eb4
Daniel <Daniel@DESKTOP-H4HQJ45>
2020-04-13 21:39:34 +0200

    .

--- workspace/Sokoban/src/Main.java
+++ workspace/Sokoban/src/Main.java
@@ -1,13 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		Level lvl1 = new Level("lvl2.txt");
-		System.out.println("Input");
-		lvl1.drawLevelTiles();
-		System.out.println("");
-		System.out.println("Output");
 		lvl1.drawLevelObjects();
+		
+		
 	}
 }
